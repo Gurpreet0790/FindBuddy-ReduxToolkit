4438872aea8894ddff32bd5ba39eb50798b51d1d
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-01-29 11:48:21 -0800

    Adding Intial Setup

---
+++ Documents/ReactNativeProjects/FindBuddyReduxToolkit/src/util/Strings.js
@@ -0,0 +1,9 @@
+export const STRINGS = {
+  // TODO: All strings should go here
+  Api_endpoints: {
+    BASE_URL: 'https://fakestoreapi.com/users',
+    USER_LIST: '?results=5',
+  },
+};
+
+export default STRINGS;
